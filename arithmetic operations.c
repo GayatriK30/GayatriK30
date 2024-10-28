fcb4251c9b5a9bb3e8c7503fec84b68d648a95e8
GayatriK30 <gayatrikute77@gmail.com>
2024-10-28 14:59:12 +0530

    Update

---
+++ arithmetic operations.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main() {
+     int a,b;
+     a=13;
+     b=17;
+     //operationforaddition//
+     printf("a+b=%d\n",a+b);
+     //operationforsubtraction//
+     printf("a-b=%d\n",a-b);
+     //operationformultiplication//
+     printf("a*b=%d\n",a*b);
+     //operationfordivision//
+     printf("a/b=%d\n",a/b);
+     //operationformodulus//
+     printf("a%b=%d\n",a%b);
+     return 0;
+     }
+     
+   OUTPUT
+    
+   a+b= 30
+   a-b= -4
+   a*b= 221
+   a/b= 0  
