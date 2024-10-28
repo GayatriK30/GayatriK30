a2333ad2f5a53899cf6baa9d3feceae5ebd428ce
GayatriK30 <gayatrikute77@gmail.com>
2024-10-28 15:02:30 +0530

    Update

---
+++ logical operation.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main() {
+     int a,b;
+     a=13;
+     b=17;
+     //operationforAND//
+     printf("a&&b=%d\n",a&&b);
+     //operationforOR//
+     printf("a||b=%d\n",a||b);
+     //operationforNOR//
+     printf("a!=b=%d\n",a!=b);
+     return 0;
+     }
+     
+     
+    OUTPUT 
+     a&&b = 1
+     a||b = 1
+     a!=b = 1
